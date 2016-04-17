aea45f892ba202d4b6da0575cf0e8d745affe416
Aman Kumar <firelord.xda@gmail.com>
2016-04-17 18:53:41 +0530

    hl3g: changes done to use samsung exynos by cm team

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,14 @@ TARGET_BOOTLOADER_BOARD_NAME := universal5260
 TARGET_BOARD_PLATFORM_GPU := mali-T624
 TARGET_NO_RADIOIMAGE := true
 TARGET_NO_BOOTLOADER := true
+TARGET_SLSI_VARIANT := cm
+TARGET_SOC := exynos5260
+
+# RENDERSCRIPT
+BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a15
+
+# LibVideoCodec
+COMMON_GLOBAL_CFLAGS += -DSOC_EXYNOS5430
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/hl3g/include
@@ -29,6 +37,9 @@ BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/hl3g
 TARGET_KERNEL_CONFIG := cyanogenmod-exynos5260-hl3gxx_00_defconfig
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := samsung
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
@@ -70,41 +81,77 @@ BOARD_RECOVERY_SWIPE := true
 # GPU
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/samsung/hl3g/egl/egl.cfg
-OVERRIDE_RS_DRIVER := libRSDriverArm.so
+#OVERRIDE_RS_DRIVER := libRSDriverArm.so
+
+# frameworks/native/services/surfaceflinger
+# Android keeps 2 surface buffers at all time in case the hwcomposer
+# misses the time to swap buffers (in cases where it takes 16ms or
+# less). Use 3 to avoid timing issues.
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Exynos display
 BOARD_USES_VIRTUAL_DISPLAY := true
+
+# HWCServices
 BOARD_USES_HWC_SERVICES := true
 
-# Audio
-BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
+# HDMI
+BOARD_HDMI_INCAPABLE := true
+
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+
+# (G)SCALER
+BOARD_USES_SCALER := true
+BOARD_USES_DT := true
+BOARD_USES_DT_SHORTNAME := true
+
+# Keymaster
+BOARD_USES_TRUST_KEYMASTER := true
+
+# Samsung LSI OpenMAX
+COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED
+
+# Samsung Seiren audio
+BOARD_USE_ALP_AUDIO := true
+BOARD_USE_SEIREN_AUDIO := true
+
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := true
+BOARD_USE_QOS_CTRL := false
+BOARD_USE_VP8ENC_SUPPORT := true
 
 # Camera
 USE_CAMERA_STUB := true
-
-# healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd-hl3g
+BOARD_NEEDS_MEMORYHEAPION := true
+BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
 
 # CMHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/hl3g/cmhw/
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/
 
 # Radio
-BOARD_RIL_CLASS := ../../../device/samsung/hl3g/ril
+#BOARD_VENDOR := samsung
+#BOARD_RIL_CLASS := ../../../device/samsung/hl3g/ril
 #BOARD_PROVIDES_LIBRIL := true
-BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet0"
-
-# Pre-L Compatibility
-COMMON_GLOBAL_CFLAGS += \
-    -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL \
-    -DENABLE_NON_PIE_SUPPORT
-
-# Legacy MMAP for pre-lollipop blobs
-# (needed by mcDriverDaemon which in turn is needed by cbd)
-BOARD_USES_LEGACY_MMAP := true
+#BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet0"
 
 # Sensors
 TARGET_NO_SENSOR_PERMISSION_CHECK := true
 
+# Webkit
+ENABLE_WEBGL := true
+
 # WFD
-BOARD_USES_WFD_SERVICE := true
+BOARD_USES_WFD := true
 
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
@@ -127,4 +174,3 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-
