14b56c3310bf6dc4cf85cf89cd2ef538f27109fc
Aman Kumar <firelord.xda@gmail.com>
2016-05-19 01:00:26 +0530

    hl3g: Add recovery fstab path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@ TARGET_OTA_ASSERT_DEVICE := hl3g
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod-exynos5260-hl3gxx_00_defconfig
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/hl3g/rootdir/fstab.universal5260
+
 # RIL
 #BOARD_RIL_CLASS := ../../../device/samsung/hl3g/ril
 BOARD_MODEM_TYPE := xmm6262
